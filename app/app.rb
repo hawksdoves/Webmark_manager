e3bf72b2419e5a034927e1c35bd35790fac40dfc
phoebe <phoebe.greig@gmail.com>
2016-03-22 13:21:42 +0000

    added development and testing environments

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,10 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
   get '/links' do
     @links = Link.all
     erb :links
